6df44c28a67d7feb13999ed54b6bffc8105800c3
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-02-02 01:27:28 -0800

    1066

---
+++ URI/Even, Odd, Positive and Negative.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int i,count1=0,count2=0,count3=0,count4=0;
+    for(i=1;i<=5;i++)
+    {
+        int a;
+        scanf("%d",&a);
+        if(a%2 == 0)
+            count1++;
+        if(a%2 != 0)
+            count2++;
+        if(a>0)
+            count3++;
+        if(a<0)
+            count4++;
+    }
+    printf("%d valor(es) par(es)\n",count1);
+    printf("%d valor(es) impar(es)\n",count2);
+    printf("%d valor(es) positivo(s)\n",count3);
+    printf("%d valor(es) negativo(s)\n",count4);
+    return 0;
+}
