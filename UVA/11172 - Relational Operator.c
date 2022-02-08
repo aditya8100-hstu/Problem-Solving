9c594bc1283193246f8940b978e35d8138764bbf
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-02-08 01:01:15 -0800

    11172

---
+++ UVA/11172 - Relational Operator.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        int a,b;
+        scanf("%d%d",&a,&b);
+        if(a>b)
+            printf(">\n");
+        else if(a<b)
+            printf("<\n");
+        else
+            printf("=\n");
+    }
+    return 0;
+}
