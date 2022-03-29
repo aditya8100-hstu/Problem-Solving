af9da7bff96fd52588fc19bf46db3bf4819ef274
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-03-29 11:52:11 +0600

    CHEFBARBER - Beginner Level

---
+++ CodeCheff/Chef and the Hair Salon.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,i;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        int N,M;
+        cin>>N>>M;
+        int r = N*M;
+        cout<<r<<endl;
+    }
+    return 0;
+}
