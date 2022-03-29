6434501fdd814b692b6890bcc9269ab501138568
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-03-29 11:42:37 +0600

    THREETOPICS - Beginner Level

---
+++ CodeCheff/The Three Topics.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c,x;
+   cin>>a>>b>>c>>x;
+   if(x==a)
+        cout<<"Yes"<<endl;
+   else if(x==b)
+    cout<<"Yes"<<endl;
+    else if(x==c)
+    cout<<"Yes"<<endl;
+   else
+    cout<<"No"<<endl;
+   return 0;
+}
