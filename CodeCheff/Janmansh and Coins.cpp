3e6fc32a2402520309fe170721ce7e18065846fa
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-03-30 18:17:42 +0600

    JCOINS - Beginner Level

---
+++ CodeCheff/Janmansh and Coins.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,t;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        int r = (x*10) +(y*5);
+        cout<<r<<endl;
+    }
+    return 0;
+}
+
