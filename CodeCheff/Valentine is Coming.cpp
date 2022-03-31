14bac6f826821cb90b3d26a76d18650984fa3207
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-03-31 10:56:58 +0600

    VALENTINE - Beginner Level

---
+++ CodeCheff/Valentine is Coming.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,t;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        int r = x/y;
+        cout<<r<<endl;
+    }
+    return 0;
+}
