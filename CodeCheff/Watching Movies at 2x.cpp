d05a1232b5429885076577aec9da374eeeae8bc1
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-03-31 10:51:53 +0600

    MOVIE2X - Beginner Level

---
+++ CodeCheff/Watching Movies at 2x.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+        int x,y;
+        cin>>x>>y;
+        int r = y/2;
+        int fr = x-r;
+        cout<<fr<<endl;
+        return 0;
+}
