d4ca7db862bb12ff06b9e6005dff8767441ee8a5
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-03-31 18:28:10 +0600

    NOTEBOOK - Beginner Level

---
+++ CodeCheff/Count the Notebooks.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,t;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        int n;
+        cin>>n;
+        int r = (1000/100)*n;
+        cout<<r<<endl;
+    }
+    return 0;
+}
