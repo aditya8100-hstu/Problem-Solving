d7882ff1ee70799ce0f0997a9f24a8b3160087ea
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-03-31 18:34:50 +0600

    TYRE - Beginner Level

---
+++ CodeCheff/Tyre problem.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,t;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        int n,m;
+        cin>>n>>m;
+        int r = (n*2)+(m*4);
+        cout<<r<<endl;
+    }
+    return 0;
+}
