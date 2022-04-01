2c723002bde9fe29f2cecf17df05aa58ec087043
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-04-01 10:08:12 +0600

    BATTERYLOW - Beginner Level

---
+++ CodeCheff/Battery Low.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,i;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        int x;
+        cin>>x;
+        if(x<=15)
+            cout<<"Yes"<<endl;
+        else
+            cout<<"No"<<endl;
+    }
+    return 0;
+}
