b6d1c434b7cacac935ed8f9852e794409dd0ad5b
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-04-01 10:18:04 +0600

    SHOPCHANGE - Beginner Level

---
+++ CodeCheff/Shopping Change.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,t;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        int x;
+        cin>>x;
+        int r = 100-x;
+        cout<<r<<endl;
+    }
+    return 0;
+}
