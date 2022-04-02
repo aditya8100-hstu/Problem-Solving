fdd0fb26e6d940076cb58859f3ba5756c90e7fe6
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-04-02 13:40:43 +0600

    INCRIQ - Beginner Level

---
+++ CodeCheff/Increase IQ.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    int r = x+7;
+    if(r>170)
+        cout<<"Yes"<<endl;
+    else
+        cout<<"No"<<endl;
+    return 0;
+}
