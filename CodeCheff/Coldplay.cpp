cb7196c7bd429f816fe7583e280d4e0075e85c4d
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-04-05 17:50:32 +0600

    SLOOP - Beginner Level

---
+++ CodeCheff/Coldplay.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,t;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        int m,s;
+        cin>>m>>s;
+        int r = m/s;
+        cout<<r<<endl;
+    }
+    return 0;
+}
