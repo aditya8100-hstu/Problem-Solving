1dae60c39b57102fd4c87c28bc6e4b793f37f66b
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-04-07 15:08:52 +0600

    CRED - running contest problem

---
+++ CodeCheff/Earn CRED Credits.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,t;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        int n;
+        cin>>n;
+        if(n>=750)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+    return 0;
+}
