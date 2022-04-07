59edebcdcc93c409c87ff32bbaaf24c723e9926f
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-04-07 15:04:40 +0600

    CODEA1 - running contest problem

---
+++ CodeCheff/First Day.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   string str;
+    getline(cin,str);
+    if(str == "Crypto")
+        cout<<"Moon"<<endl;
+    else
+        cout<<"Space"<<endl;
+    return 0;
+}
