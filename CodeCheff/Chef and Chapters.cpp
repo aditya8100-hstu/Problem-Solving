c54030182fb774519c62885f27ebb8a463b525c2
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-05-02 09:52:04 +0600

    SEMCOURSES

---
+++ CodeCheff/Chef and Chapters.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,t;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        int x,y,z;
+        cin>>x>>y>>z;
+        int r = x*y*z;
+        cout<<r<<endl;
+    }
+    return 0;
+}
