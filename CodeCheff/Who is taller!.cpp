0f3ef6b6b9efcc876eac367d08d6636752b0c6d3
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-05-02 09:47:00 +0600

    TALLER

---
+++ CodeCheff/Who is taller!.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,t;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        if(x>y)
+            cout<<"A"<<endl;
+        else
+            cout<<"B"<<endl;
+    }
+    return 0;
+}
