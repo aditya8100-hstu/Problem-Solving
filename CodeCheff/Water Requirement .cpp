dac32a292d78b1e43febab2cbac34c71569100f7
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-05-03 09:54:31 +0600

    WATERREQ

---
+++ CodeCheff/Water Requirement .cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,t;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        int n;
+        cin>>n;
+        int r = 2*n;
+        cout<<r<<endl;
+    }
+    return 0;
+}
