d5fba353ba7ecbc889fd7136ee2d947929813ff8
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-05-14 17:23:14 +0600

    SUBSCRIBE

---
+++ CodeCheff/Get Subscription.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,t;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        int x;
+        cin>>x;
+        if(x>30)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+    return 0;
+}
