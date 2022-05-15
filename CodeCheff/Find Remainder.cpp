40d3867fd1eb112d5648b715105db9bcdb6c2a89
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-05-15 13:15:52 +0600

    FLOW002

---
+++ CodeCheff/Find Remainder.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,t;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        int r = a%b;
+        cout<<r<<endl;
+    }
+    return 0;
+}
