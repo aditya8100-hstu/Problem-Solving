830f65620f0d537147422507923953aea2d4c69d
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-05-18 10:20:12 +0600

    APPLORNG

---
+++ CodeCheff/Apples and Oranges.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    int a,b;
+    cin>>x>>a>>b;
+    int r = (a*1)+(b*1);
+    if(x>=r)
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+    return 0;
+}
