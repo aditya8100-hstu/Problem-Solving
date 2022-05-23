2bcb13173209394d2fd91b7b10efa482ddbe04be
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-05-23 22:06:50 +0600

    DECINC

---
+++ CodeCheff/Decrement OR Increment.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    if(a%4==0)
+        cout<<(a+1)<<endl;
+    else
+        cout<<(a-1)<<endl;
+    return 0;
+}
