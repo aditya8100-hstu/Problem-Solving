9c3054b6be77a7c7870903f1d884066271bbe075
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-05-26 09:03:05 +0600

    FLOW008

---
+++ CodeCheff/Helping Chef.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,t;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        int a;
+        cin>>a;
+        if(a<10) cout<<"Thanks for helping Chef!"<<endl;
+        else cout<<"-1"<<endl;
+    }
+    return 0;
+}
