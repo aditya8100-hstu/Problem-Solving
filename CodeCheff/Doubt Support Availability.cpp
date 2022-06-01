8d4803399b6129b45a3db83a573d6b2055c77d72
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-06-01 18:55:34 +0600

    DOUBTSUPPORT

---
+++ CodeCheff/Doubt Support Availability.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int d;
+    cin>>d;
+    if(d<=1600)
+        cout<<"Yes"<<endl;
+    else
+        cout<<"No"<<endl;
+    return 0;
+}
