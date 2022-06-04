050f1a42d32befa4490b1bdc203233e21a714730
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-06-04 11:26:44 +0600

    WORDLE

---
+++ CodeCheff/Wordle.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s,t;
+        cin>>s>>t;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i] == t[i])
+              cout<<'G';
+            else
+                cout<<'B';
+        }
+        cout<<endl;
+    }
+    return 0;
+}
