9c55286c08d35886a97df3bc9d7bfe3c2532e9a8
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-06-05 21:37:41 +0600

    SONGS - Running contest problem

---
+++ CodeCheff/Playlist.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        int r = n/(x*3);
+        cout<<r<<endl;
+    }
+    return 0;
+}
