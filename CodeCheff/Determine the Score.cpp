6ee305a810e9b96d9af59080d1d5fbfa5952bf15
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-06-21 10:31:23 +0600

    DETSCORE

---
+++ CodeCheff/Determine the Score.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+        int result = (a/10)*b;
+        cout<<result<<endl;
+    }
+    return 0;
+}
