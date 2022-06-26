32880d6d14e22b99a94f9d6b0b4befed4a8662b9
Aditya Chakrabortty <58524511+aditya8100-hstu@users.noreply.github.com>
2022-06-26 20:56:01 +0600

    FIT - running contest problem

---
+++ CodeCheff/Fitness.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x;
+        cin>>x;
+        int result = (x*2)*5;
+        cout<<result<<endl;
+    }
+    return 0;
+}
